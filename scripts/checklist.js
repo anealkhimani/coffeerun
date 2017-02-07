7cb9d117d9eb9d54d3543a05abe6ea034e29c7e3
Aneal Khimani <akhima2@emory.edu>
2017-02-07 14:53:07 -0500

    First Commit

---
+++ scripts/checklist.js
@@ -0,0 +1,88 @@
+(function(window) {
+  var App = window.App || {};
+  var $ = window.jQuery;
+
+  function CheckList(selector) {
+    if(!selector) {
+      throw new Error("No Selector provided");
+    }
+
+    this.$element = $(selector);
+    if(this.$element.length === 0) {
+      throw new Error ("Could not find element with selector: " + selector);
+    }
+  }
+
+  CheckList.prototype.addClickHandler = function(fn) {
+    this.$element.on('click','input', function(event) {
+      var email = event.target.value;
+      this.removeRow(email);
+      fn(email);
+    }.bind(this));
+  };
+
+  CheckList.prototype.addRow = function(coffeeOrder) {
+    // Remove any existing orders for this email address
+    this.removeRow(coffeeOrder.emailAddress);
+    // Create new instance of a row passing in the coffeeOrder
+    var rowElement = new Row(coffeeOrder);
+
+    // Add the new row instance's $element property to the CheckList
+    this.$element.append(rowElement.$element);
+  }
+
+  CheckList.prototype.removeRow = function(email) {
+    this.$element
+      .find('[value="' + email + '"]')
+      .closest('[data-coffee-order="checkbox"]')
+      .remove();
+  }
+
+  function Row(coffeeOrder) {
+    var $div = $('<div></div>', {
+      'data-coffee-order': 'checkbox',
+      'class': 'checkbox'
+    });
+
+    var $label = $('<label></label>');
+
+    var $checkbox = $('<input></input>', {
+      type: 'checkbox',
+      value: coffeeOrder.emailAddress
+    });
+
+
+    var description = '[' + coffeeOrder.strength + 'x] ';
+    description += coffeeOrder.size + ' ';
+    if(coffeeOrder.flavor) {
+      var color;
+      switch(coffeeOrder.flavor) {
+        case 'caramel':
+          color = 'brown';
+          break;
+        case 'almond':
+          color = 'yellow';
+          break;
+        case 'mocha':
+          color = 'cyan';
+          break;
+      }
+      description += '<span style="background-color:'+color+';">'+ coffeeOrder.flavor + '</span> ';
+    }
+
+    description += coffeeOrder.coffee + ' ';
+    description += '(' + coffeeOrder.emailAddress + ')';
+
+    $label.append($checkbox);
+    $label.append(description);
+    $div.append($label);
+    this.$element = $div;
+
+  }
+
+
+
+
+  App.CheckList = CheckList;
+  window.App = App;
+})(window);
